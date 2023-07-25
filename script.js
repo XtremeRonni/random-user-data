dfee4cfd687ecd03073a551644d30a91f1659f4e
Abheekrishna <abheekrishna17@gmail.com>
2023-07-25 18:23:02 +0530

    Styling data

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const url = 'https://randomuser.me/api/';
 fetch(url)
 .then(response => response.json())
 .then(json => {
-    console.log(json);
+    console.log(json.results);
     const tableDiv = document.querySelector('.table');
 
     const imageDiv = document.querySelector('.profile');
@@ -16,9 +16,10 @@ fetch(url)
 
     tableDiv.innerHTML = `
         <p class="placeholder">Name: <span class="name">${json.results[0].name.title} ${json.results[0].name.first} ${json.results[0].name.last}</p>
-        <p class="placeholder">Name: <span class="gender">${json.results[0].gender}</p>
-        <p class="placeholder">Name: <span class="email">${json.results[0].email}</p>
-        <p class="placeholder">Name: <span class="phone">${json.results[0].cell}</p>
-        <p class="placeholder">Name: <span class="age">${json.results[0].dob.age}</p>
+        <p class="placeholder">Gender: <span class="gender">${json.results[0].gender}</p>
+        <p class="placeholder">Email: <span class="email">${json.results[0].email}</p>
+        <p class="placeholder">Phone: <span class="phone">${json.results[0].cell}</p>
+        <p class="placeholder">Age: <span class="age">${json.results[0].dob.age}</p>
+        <p class="placeholder">Country: <span class="place">${json.results[0].location.country}</p>
         `
 })
