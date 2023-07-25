9554bd1762076c2e5ff6fd87312e316aa75034a9
Abheekrishna <abheekrishna17@gmail.com>
2023-07-25 18:08:11 +0530

    Added info

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const containerDiv = document.querySelector('.container');
-const tableDiv = document.querySelector('.table');
-const imageDiv = document.querySelector('profile');
+
+
 
 const url = 'https://randomuser.me/api/';
+
 
 fetch(url)
 .then(response => response.json())
 .then(json => {
-    console.log(json.results[0].name.last);
+    console.log(json);
+    const tableDiv = document.querySelector('.table');
+
+    const imageDiv = document.querySelector('.profile');
+    imageDiv.src = `${json.results[0].picture.large}`;
+
+    tableDiv.innerHTML = `
+        <p class="placeholder">Name: <span class="name">${json.results[0].name.title} ${json.results[0].name.first} ${json.results[0].name.last}</p>
+        <p class="placeholder">Name: <span class="gender">${json.results[0].gender}</p>
+        <p class="placeholder">Name: <span class="email">${json.results[0].email}</p>
+        <p class="placeholder">Name: <span class="phone">${json.results[0].cell}</p>
+        <p class="placeholder">Name: <span class="age">${json.results[0].dob.age}</p>
+        `
 })
